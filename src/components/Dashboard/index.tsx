f684dafe7a16a353a93df550587093a7371bae9b
Alexander kruger <alex.bigbox@gmail.com>
2021-05-12 10:45:07 -0300

    Versão 2.0 problema no modal

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import { Container } from "./style";
+import { Summary } from "../Summary";
+import { TransactionsTable } from "../TransactionsTable";
+
+export function Dashboard() {
+    return (
+        <Container>
+            <Summary />
+            <TransactionsTable />
+        </Container>
+    );
+}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import logoImg from '../../assets/logo.svg'
+import { Container, Content } from './style'
+
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
+    return (
+        <Container>
+            <Content>
+                <img src={logoImg} alt="BB Money" />
+                <button type="button" onClick={onOpenNewTransactionModal}>
+                        Nova Transação
+                </button>
+
+                
+            </Content>
+        </Container>
+    )
+}
